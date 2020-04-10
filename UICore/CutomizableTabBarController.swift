4aa30aedf4c1f75887c7bfde3dc8e85d284115ab
José Ramos <jrcmramos@gmail.com>
2020-04-10 17:31:14 +0200

    Update code to Swift 5.2

--- UICore/CutomizableTabBarController.swift
+++ UICore/CutomizableTabBarController.swift
@@ -12,10 +12,10 @@ public protocol CustomizableTabBarController where Self: UIViewController {}
 
 public extension CustomizableTabBarController {
     
-    func configureTabBar(imageWithName: String, selectedImageWithName: String) {
+    func configureTabBar(image: UIImage, selectedImage: UIImage) {
         let tabBarItem = UITabBarItem(title: nil,
-                                      image: UIImage(named: imageWithName)?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal),
-                                      selectedImage: UIImage(named: selectedImageWithName)?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal))
+                                      image: image.withRenderingMode(.alwaysOriginal),
+                                      selectedImage: selectedImage.withRenderingMode(.alwaysOriginal))
         tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
         tabBarItem.title = nil
         self.tabBarItem = tabBarItem
